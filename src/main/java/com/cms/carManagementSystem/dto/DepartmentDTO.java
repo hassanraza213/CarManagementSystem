dca5e42262d721880b0bc59e5c9cfe1607cdc50c
Hassan Raza <19011519-012@uog.edu.pk>
2025-01-20 14:38:45 +0500

    managed conflicts

--- src/main/java/com/cms/carManagementSystem/dto/DepartmentDTO.java
+++ src/main/java/com/cms/carManagementSystem/dto/DepartmentDTO.java
@@ -3,7 +3,6 @@ package com.cms.carManagementSystem.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
