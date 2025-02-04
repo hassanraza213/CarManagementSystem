ab6fe119d99afcaf2c5cb240da8b04231fdc27e4
Hassan Raza <19011519-012@uog.edu.pk>
2025-02-04 09:24:37 +0500

    Stop tracking src directory and update .gitignore

--- src/main/java/com/cms/carManagementSystem/repository/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.cms.carManagementSystem.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cms.carManagementSystem.entity.Employee;
-
-public interface EmployeeRepo extends JpaRepository<Employee, Long>{
-
-}
