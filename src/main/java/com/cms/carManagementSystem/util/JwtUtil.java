9d1b35cba669e21216b432f2a486c07d4e830723
Hassan Raza <19011519-012@uog.edu.pk>
2025-03-05 10:38:52 +0500

    Moved secret key from JwtUtil to Application.properties

--- src/main/java/com/cms/carManagementSystem/util/JwtUtil.java
+++ src/main/java/com/cms/carManagementSystem/util/JwtUtil.java
@@ -3,48 +3,48 @@ package com.cms.carManagementSystem.util;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
 
+@Data
 @Slf4j
 @Component
 public class JwtUtil {
 
-    private static final String SECRET_KEY = "your-very-long-secret-key-for-signing-jwts-at-least-256-bits-long";
-    public static final long EXPIRATION_TIME = 1000 * 60 * 60; // 1 hour
+    @Value("${jwt.secret}")
+    private String secretKey;
 
-    private final SecretKey key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    @Value("${jwt.expiration}")
+    private long expirationTime;
+
+    private SecretKey key;
+
+    @PostConstruct
+    private void init() {
+        key = Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
 
     public String generateToken(String userName) {
         log.info("Generating JWT for username: {}", userName);
-        return Jwts.builder()
-                .subject(userName)
-                .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(key)
-                .compact();
+        return Jwts.builder().subject(userName).issuedAt(new Date()).expiration(new Date(System.currentTimeMillis() + expirationTime)).signWith(key).compact();
     }
 
     public String extractUsername(String token) {
         log.info("Extracting username from JWT");
-        Claims claims = Jwts.parser()
-                .verifyWith(key)
-                .build()
-                .parseSignedClaims(token)
-                .getPayload();
+        Claims claims = Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload();
         return claims.getSubject();
     }
 
     public boolean validateToken(String token) {
         try {
             log.info("Validating JWT");
-            Jwts.parser()
-                    .verifyWith(key)
-                    .build()
-                    .parseSignedClaims(token);
+            Jwts.parser().verifyWith(key).build().parseSignedClaims(token);
             return true;
         } catch (Exception e) {
             log.error("Invalid JWT: {}", e.getMessage());
